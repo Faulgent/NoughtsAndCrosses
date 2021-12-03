f9b578ba3496567413d96edfe664073ce7bec908
Faulgent <faulgent@gmail.com>
2021-12-03 15:00:58 +0000

    GameActivity is created

--- app/src/main/java/com/margaritasoft/noughtsandcrosses/GameActivity.kt
+++ app/src/main/java/com/margaritasoft/noughtsandcrosses/GameActivity.kt
@@ -1,11 +1,85 @@
 package com.margaritasoft.noughtsandcrosses
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class GameActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val scale = intent.getSerializableExtra("scale")
+        val viewModel = GameViewModel()
+        viewModel.startNewGame(scale as Scale)
+        viewModel.field.observe(this, Observer {drawLayout(it, viewModel::pressed) })
+        viewModel.win.observe(this, Observer {displayWinWindow(viewModel)})
+
+    }
+    fun displayWinWindow(viewModel: GameViewModel) {
+        if (viewModel.win.value == true) {
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("Congratulations!")
+                .setMessage("You won!")
+                .setPositiveButton("Start new game") { _: DialogInterface, i: Int -> finish() }
+                .show()
+        }
+    }
+
+    private fun drawLayout(
+        field: Array<Array<Symbol>>,
+        onPressed: (x: Int, y: Int) -> Unit
+    ) {
+        val layout = createLayout()
+        val scale = field.size
+
+        for (y in 0 until scale) {
+            layout.addView(createRow(y, scale, field, onPressed))
+        }
+        setContentView(layout)
+    }
+
+    private fun createRow(
+        rowIndex: Int,
+        size: Int,
+        field: Array<Array<Symbol>>,
+        onPressed: (x: Int, y: Int) -> Unit
+    ): LinearLayout {
+        val row = LinearLayout(this)
+        row.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        for (x in 0 until size) {
+            val button = Button(this)
+            button.layoutParams = LinearLayout.LayoutParams(0, 250).apply {
+                weight = 1f
+            }
+
+            when (field[x][rowIndex]) {
+                Symbol.CROSS -> button.setBackgroundResource(R.drawable.ic_cross)
+                Symbol.NOUGHT -> button.setBackgroundResource(R.drawable.ic_circle)
+                Symbol.EMPTY -> button.setOnClickListener {
+                    onPressed(x, rowIndex)
+                }
+            }
+
+            row.addView(button)
+            row.gravity = Gravity.CENTER
+        }
+        return row
+    }
+
+    private fun createLayout(): LinearLayout {
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
+        layout.gravity = Gravity.CENTER
+        return layout
     }
 }
